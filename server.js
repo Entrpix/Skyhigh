de34ac42d7e0dd2570f121da3b23e6486ae41f3b
Scaratek <153395462+Entrpix@users.noreply.github.com>
2024-07-31 21:18:00 -0400

    express >

--- server.js
+++ server.js
@@ -1,54 +1,27 @@
 import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
-import fastifyStatic from "@fastify/static";
 import { fileURLToPath } from "node:url";
-import { createServer } from "http";
 import wisp from "wisp-server-node";
+import { createServer } from "http";
 import { join } from "node:path";
-import Fastify from "fastify";
+import express from "express";
 
-const fastify = Fastify({
-	serverFactory: (handler) => {
-		return createServer()
-			.on("request", (req, res) => {
-					handler(req, res);
-			})
-			.on("upgrade", (req, socket, head) => {
-					wisp.routeRequest(req, socket, head);
-			});
-	},
+const app = express();
+
+app.use(express.static(join(fileURLToPath(new URL(".", import.meta.url)), "./public")));
+
+app.use("/sky", express.static(join(fileURLToPath(new URL(".", import.meta.url)), "./dist")));
+
+app.use("/baremux", express.static(baremuxPath));
+app.use("/epoxy", express.static(epoxyPath));
+app.use("/libcurl", express.static(libcurlPath));
+
+const server = createServer(app);
+server.on("upgrade", (req, socket, head) => {
+	wisp.routeRequest(req, socket, head);
 });
 
-fastify.register(fastifyStatic, {
-	root: join(fileURLToPath(new URL(".", import.meta.url)), "./public"),
-	decorateReply: false,
-});
-
-fastify.register(fastifyStatic, {
-	root: join(fileURLToPath(new URL(".", import.meta.url)), "./dist"),
-	prefix: "/sky/",
-	decorateReply: false,
-});
-
-fastify.register(fastifyStatic, {
-	root: baremuxPath,
-	prefix: "/baremux/",
-	decorateReply: false,
-});
-
-fastify.register(fastifyStatic, {
-	root: epoxyPath,
-	prefix: "/epoxy/",
-	decorateReply: false,
-});
-
-fastify.register(fastifyStatic, {
-	root: libcurlPath,
-	prefix: "/libcurl/",
-	decorateReply: false,
-});
-
-fastify.listen({
-	port: 5000
+server.listen(3000, () => {
+	console.log("Server listening on port 3000");
 });
